ec3b16883d07071b6023e18fab3e52e758e4d68e
chukugodson <chukugodson87@gmail.com>
2022-06-24 17:38:35 +0100

    First commit

---
+++ calc.js
@@ -0,0 +1,8 @@
+window.prompt("Please select the operation you will like to complete from 1 to 5 and press ok 1. Add(+) 2. Subtract(-) 3. Multiply(x) 4. Divide(/)");
+
+var num1 = window.prompt("Enter first number");
+var num2 = window.prompt("Enter second number");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
