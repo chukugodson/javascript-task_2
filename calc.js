5981dc07d698810995def8047280fabb9b6dc183
Chuku Godson Hachinyerum <chukugodson87@gmail.com>
2022-06-26 23:23:16 +0100

    First commit

--- calc.js
+++ calc.js
@@ -1,8 +1,30 @@
-window.prompt("Please select the operation you will like to complete from 1 to 5 and press ok 1. Add(+) 2. Subtract(-) 3. Multiply(x) 4. Divide(/)");
+var select = window.prompt("Please select the operation you will like to complete from 1 to 4 and press OK :   1. Add(+)   2. Subtract(-)   3. Multiply(x)    4. Divide(/)");
 
-var num1 = window.prompt("Enter first number");
-var num2 = window.prompt("Enter second number");
-
-num1 = parseInt(num1);
-num2 = parseInt(num2);
+if (select == 1){
+    var num1 = window.prompt("Enter first number");
+    var num2 = window.prompt("Enter second number");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    document.write(num1, " " , "+" , " " , num2, " " , "=", " " , num1+num2)
+}else if (select == 2){
+    var num1 = window.prompt("Enter first number");
+    var num2 = window.prompt("Enter second number");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    document.write(num1, " " , "-" , " " , num2, " " , "=", " " , num1-num2)
+}else if (select == 3){
+    var num1 = window.prompt("Enter first number");
+    var num2 = window.prompt("Enter second number");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    document.write(num1, " " , "x" , " " , num2, " " , "=", " " , num1*num2)
+}else if (select == 4){
+    var num1 = window.prompt("Enter first number");
+    var num2 = window.prompt("Enter second number");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    document.write(num1, " " , "/" , " " , num2, " " , "=", " " , num1/num2)
+}else {
+    alert(" 'Invalid option'. Reload and choose from the options given.")
+}
 
